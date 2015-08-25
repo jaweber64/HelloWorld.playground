8c529e394109ecbffdd571acdade0e793968d70f
jaweber64 <jaweber64@gmail.com>
2015-08-25 17:12:38 -0500

    This is a test

---
+++ Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+"Hello World!!"
+
+"This is quite, fun!!"
+
+
+"So far understandable, but very NEW!"
